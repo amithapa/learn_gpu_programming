e41e278c9416245b2d3e72498cdd7c88f948eef2
Amit Thapa <amithapa1994@gmail.com>
2019-07-19 01:42:23 +0530

    cuda c source code

---
+++ cube.cu
@@ -0,0 +1,42 @@
+# include <stdio.h>
+
+__global__ void cube(float *d_in, float *d_out) {
+    int idx = threadIdx.x;
+    float f = d_in[idx];
+    d_out[idx] = f * f *f;
+}
+
+int main() {
+    const int ARRAY_SIZE = 164;
+    const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
+
+    float h_in[ARRAY_SIZE];
+    for(int i=0; i < ARRAY_SIZE; i++) {
+        h_in[i] = float(i);
+    }
+    float h_out[ARRAY_SIZE];
+
+    // declare memory pointers
+    float * d_in;
+    float * d_out;
+
+    // allocating memory for GPU variables
+    cudaMalloc((void **) &d_in, ARRAY_BYTES);
+    cudaMalloc((void **) &d_out, ARRAY_BYTES);
+
+    cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+
+    cube<<<1, ARRAY_SIZE>>>(d_in, d_out);
+
+    cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+
+    for(int i=0; i < ARRAY_SIZE; i++) {
+        printf("%f", h_out[i]);
+        printf(((i%4) != 3) ? "\t" : "\n");
+    }
+
+    cudaFree(d_in);
+    cudaFree(d_out);
+
+    return 0;
+}
